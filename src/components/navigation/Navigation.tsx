40cdf2c5bcecdbf35464efd813e922629e1a0c07
Carlos Javier Mathias Carcamo <carlosmts@gmail.com>
2024-03-16 03:00:24 -0300

    Active link styles.

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 interface NavigationProps {
   onLinkClick?: () => void;
@@ -20,16 +20,19 @@ const Navigation: React.FC<NavigationProps> = ({ onLinkClick, className }) => {
       role="navigation"
     >
       {links.map(([title, url]) => (
-        <Link
+        <NavLink
           to={url}
-          className={classNames(
-            'py-2 font-semibold',
-            'relative lg:after:content-[""] lg:after:h-1 lg:after:bg-purple-600 lg:after:absolute lg:after:top-full lg:after:w-full lg:after:left-0 lg:after:scale-0 lg:hover:after:scale-100'
-          )}
+          className={({ isActive }) =>
+            classNames(
+              'py-2 font-semibold',
+              'relative lg:after:content-[""] lg:after:h-1 lg:after:bg-purple-600 lg:after:absolute lg:after:top-full lg:after:w-full lg:after:left-0 lg:after:scale-0 lg:hover:after:scale-100',
+              isActive ? 'lg:after:scale-100' : ''
+            )
+          }
           onClick={onLinkClick}
         >
           {title}
-        </Link>
+        </NavLink>
       ))}
     </nav>
   );
